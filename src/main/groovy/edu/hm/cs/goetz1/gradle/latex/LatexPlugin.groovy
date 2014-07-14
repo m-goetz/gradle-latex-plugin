ee42314e20fb26f849c5393a995c3ea0cd9ac7a8
Maximilian Götz <maximilian.goetz@ymail.com>
2014-07-14 15:10:52 +0200

    pdflatexFirstRun and biber.

--- src/main/groovy/edu/hm/cs/goetz1/gradle/latex/LatexPlugin.groovy
+++ src/main/groovy/edu/hm/cs/goetz1/gradle/latex/LatexPlugin.groovy
@@ -11,81 +11,29 @@ class LatexPlugin implements Plugin<Project> {
     void apply(Project project) {
         project.extensions.create("latex", LatexPluginExtension)
         
-        def pdfLatexExecTask = project.tasks.create("basicPdfLatex", Exec.class)
-        pdfLatexExecTask.setDescription("Produces a pdf from a tex document by executing pdflatex once.")
-        pdfLatexExecTask.setGroup("LaTeX")
+        def pdflatexFirstRun = project.tasks.create("pdflatexFirstRun", Exec.class)
+        pdflatexFirstRun.setDescription("Produces a pdf from a tex document by executing pdflatex once.")
+        pdflatexFirstRun.setGroup("LaTeX")
         
-        def biberExecTask = project.tasks.create("biber", Exec.class)
-        biberExecTask.setDescription("Executes biber to process the biblography.")
-        biberExecTask.setGroup("LaTeX")
+        def biber = project.tasks.create("biber", Exec.class)
+        biber.setDescription("Runs biber to process the bibliography.")
+        biber.setGroup("LaTeX")
         
-        def pdflatexTask = project.tasks.create("pdflatex", Exec.class)
-        pdflatexTask.setDescription("Produces the final pdf from a tex document by executing pdflatex after biber.")
-        pdflatexTask.setGroup("LaTeX")
-        
-        def pdfLatexConfigTask = project.task("configPdfLatex") << {
-            def outputDirectory = new File("${project.latex.outputDir}/pdflatex")
-            outputDirectory.mkdirs();
-            def inputFile = new File("${project.latex.mainFilename}.tex")
-            pdfLatexExecTask.configure {
-                commandLine("pdflatex", "-synctex=1", "-interaction=nonstopmode", 
-                    "-output-directory=${outputDirectory}", "${inputFile}")
-                outputs.dir("${outputDirectory}")
-                inputs.files("${inputFile}")
-            }
+        project.afterEvaluate {
+            pdflatexFirstRun.commandLine "pdflatex", "-output-directory=../${project.latex.outputDir}", 
+                        "-synctex=1", "-interaction=nonstopmode", 
+                        "${project.latex.mainFilename}.tex"
+            pdflatexFirstRun.workingDir("${project.latex.sourceDir}")
+            pdflatexFirstRun.inputs.dir("${project.latex.sourceDir}")
+            pdflatexFirstRun.outputs.dir("${project.latex.outputDir}")
+            
+            biber.commandLine "biber", "${project.latex.mainFilename}"
+            biber.workingDir("${project.latex.outputDir}")
+            biber.inputs.file("${project.latex.outputDir}/${project.latex.mainFilename}.aux")
+            biber.outputs.files("${project.latex.outputDir}/${project.latex.mainFilename}.bbl", 
+                    "${project.latex.outputDir}/${project.latex.mainFilename}.blg")
         }
-        
-        def biberConfigTask = project.task("configBiber") << {
-            def outputDirectory = new File("${project.latex.outputDir}/biber")
-            outputDirectory.mkdirs();
-            def inputFile = new File("${project.latex.outputDir}/pdflatex/${project.latex.mainFilename}.bcf")
-            biberExecTask.configure {
-                commandLine("biber", "--output_directory=${outputDirectory}", 
-                        "--logfile=${project.latex.mainFilename}","${inputFile}")
-                outputs.dir("${outputDirectory}")
-                inputs.files("${inputFile}")
-            }
-        }
-        
-        def copyTask = project.tasks.create("copyBblFile", Copy.class)
-        copyTask.configure {
-            from "${project.latex.outputDir}/biber"
-            into "${project.latex.outputDir}/pdflatex"
-            include "*.bbl"
-        }
-        
-        def finalPdflatexConfigTask = project.task("configFinalPdflatex") << {
-            def outputDirectory = new File("${project.latex.outputDir}/pdflatex")
-            outputDirectory.mkdirs();
-            def inputFile = new File("${project.latex.mainFilename}.tex")
-            pdflatexTask.configure {
-                commandLine("pdflatex", "-synctex=1", "-interaction=nonstopmode", 
-                    "-output-directory=${outputDirectory}", "${inputFile}")
-                outputs.dir("${outputDirectory}")
-                inputs.files("${inputFile}")
-            }
-        }
-        
-        def deleteBblTask = project.tasks.create("deleteBblFile", Delete.class)
-        deleteBblTask.configure {
-            delete "${project.latex.outputDir}/pdflatex/${project.latex.mainFilename}.bbl"
-        }
-        
-        pdflatexTask << {
-            println deleteBblTask.targetFiles
-        }
-
-        pdfLatexExecTask.dependsOn(pdfLatexConfigTask)
-        biberExecTask.dependsOn(biberConfigTask)
-        biberExecTask.dependsOn(pdfLatexExecTask)
-        pdflatexTask.dependsOn(biberExecTask)
-        pdflatexTask.dependsOn(finalPdflatexConfigTask)
-        finalPdflatexConfigTask.dependsOn(copyTask)
-        pdflatexTask.finalizedBy(deleteBblTask)
     }
 }
 
-class LatexPluginExtension {
-    String mainFilename = "main"
-    String outputDir = "build"
-}
+
---
+++ src/main/groovy/edu/hm/cs/goetz1/gradle/latex/LatexPluginExtension.groovy
@@ -0,0 +1,7 @@
+package edu.hm.cs.goetz1.gradle.latex
+
+class LatexPluginExtension {
+    String mainFilename = "main"
+    String outputDir = "build"
+    String sourceDir = "src"
+}
