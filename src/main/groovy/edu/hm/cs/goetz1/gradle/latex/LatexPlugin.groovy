0b25bfd0f77ecd136112c838e89087470a794884
Maximilian Götz <maximilian.goetz@ymail.com>
2014-07-16 17:06:26 +0200

    Added licence information and readme file.

--- src/main/groovy/edu/hm/cs/goetz1/gradle/latex/LatexPlugin.groovy
+++ src/main/groovy/edu/hm/cs/goetz1/gradle/latex/LatexPlugin.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package edu.hm.cs.goetz1.gradle.latex
 
 import org.gradle.api.Project
@@ -9,6 +24,14 @@ import org.gradle.api.tasks.Delete
 import java.io.ByteArrayOutputStream
 import org.gradle.api.logging.LogLevel
 
+/**
+ * Plugin for Gradle to execute LaTeX builds.
+ *
+ * The plugin provides incremental builds and biblatex execution by using biber.
+ *
+ * @author: Maximilian Götz
+ *
+ */
 class LatexPlugin implements Plugin<Project> {
     
     Project project
